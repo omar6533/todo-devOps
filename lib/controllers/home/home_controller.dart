c66e2bda7b883f4a46ffa926598d7e4b8437d9ca
omar <omar.h.alsafadi@gmail.com>
2022-11-22 15:24:08 +0300

    connecting to DB and add insert function

--- lib/controllers/home/home_controller.dart
+++ lib/controllers/home/home_controller.dart
@@ -1,21 +1,36 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:mytodo/controllers/auth/signin_controller.dart';
 import 'package:mytodo/services/app_snakbar.dart';
 
 class HomeController extends GetxController {
-  getUser() async {
+  TextEditingController textEditingControllerNote = TextEditingController();
+  SinginController _singinController = Get.find();
+
+  addUserNote(String note) async {
     try {
-      User? user = await FirebaseAuth.instance.currentUser;
-      if (user != null) {
-        return user;
-      }
+      CollectionReference mycollectoin =
+          await FirebaseFirestore.instance.collection('ToDo');
+      mycollectoin.add({
+        "note": "${note}",
+        "userEmail": "${_singinController.email.text.toLowerCase()}"
+      });
     } catch (e) {
       appSnackBar('Erorr', e, Colors.red);
     }
   }
 
-  getUserNotes() {}
+  getUserNotes() {
+    User? userInfo = FirebaseAuth.instance.currentUser;
+    if (User == null) {
+      return null;
+    } else {
+      return userInfo;
+    }
+  }
 
   Future<String> getUserEmail() async {
     try {
@@ -40,6 +55,14 @@ class HomeController extends GetxController {
       }
     } catch (e) {
       return appSnackBar('Erorr', e, Colors.red);
+    }
+  }
+
+  bool validNote(String note) {
+    if (note.isEmpty) {
+      return false;
+    } else {
+      return true;
     }
   }
 }
--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -5,13 +5,20 @@ import 'package:mytodo/controllers/auth/signin_controller.dart';
 import 'package:mytodo/controllers/home/home_controller.dart';
 import 'package:mytodo/widgets/app_textfiled.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
   Widget build(BuildContext context) {
+    GlobalKey<FormState> formeState = GlobalKey<FormState>();
     HomeController _homeController = Get.put(HomeController());
     SinginController _singInController = Get.find();
+
     return Scaffold(
       drawer: Drawer(
           child: Padding(
@@ -19,9 +26,7 @@ class Home extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.only(top: 50),
           child: Column(
-            children: [
-              Text("Hi ${_homeController.getUserEmail().toString()}"),
-            ],
+            children: [Text("hi  ${''}")],
           ),
         ),
       )),
@@ -42,21 +47,85 @@ class Home extends StatelessWidget {
         padding: const EdgeInsets.all(16.0),
         child: Center(
             child: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+          child: ListView(
+            shrinkWrap: true,
             children: [
-              AppTextField(
-                hinttext: 'what are you going to do?',
-                rightIcon: IconButton(
-                  icon: const Icon(Icons.add),
-                  onPressed: () {},
-                ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Form(
+                    key: formeState,
+                    child: AppTextField(
+                      textEditingController:
+                          _homeController.textEditingControllerNote,
+                      validator: (val) {
+                        bool isvalid = _homeController.validNote(val);
+                        if (isvalid == false) {
+                          return " note can't be empty";
+                        }
+                      },
+                      onSaved: (val) {
+                        _homeController.textEditingControllerNote.text = val;
+                      },
+                      hinttext: 'what are you going to do?',
+                      rightIcon: IconButton(
+                        icon: const Icon(Icons.add),
+                        onPressed: () async {
+                          var currentState = formeState.currentState;
+                          if (currentState!.validate()) {
+                            currentState.save();
+                            print('-------------------------');
+                            print(
+                                _homeController.textEditingControllerNote.text);
+                            _homeController.addUserNote(
+                                _homeController.textEditingControllerNote.text);
+                          }
+                        },
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 32,
+                  ),
+                  ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: 2,
+                    itemBuilder: (context, index) => Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(12),
+                        // color: Colors.blue,
+                      ),
+                      margin: const EdgeInsets.all(8),
+                      child: ListTile(
+                        leading: Icon(Icons.note),
+                        title: Text('hello'),
+                        trailing: IconButton(
+                            onPressed: () {
+                              print('object');
+                            },
+                            icon: Icon(Icons.delete)),
+                      ),
+                    ),
+                  )
+                ],
               ),
-              SingleChildScrollView()
             ],
           ),
         )),
       ),
     );
   }
+}
+
+Future<String> getUserEmail() async {
+  HomeController _homeController = Get.find();
+
+  String email = await _homeController.getUserEmail().then((value) {
+    return value;
+  });
+
+  print(email);
+
+  return email.toString();
 }
